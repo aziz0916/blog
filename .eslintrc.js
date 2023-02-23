0353a34442937fc846d740ae74dee3a15d986723
aziz0916 <aziz0916@yahoo.com.tw>
2023-02-23 14:31:31 +0800

    Format code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
   extends: [
-    "eason",
-    "next/core-web-vitals",
-    "plugin:prettier/recommended", // Make this the last element so prettier config overrides other formatting rules
+    'eason',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
   ],
   rules: {
-    "jsx-a11y/anchor-is-valid": [
-      "error",
+    'jsx-a11y/anchor-is-valid': [
+      'error',
       {
-        components: ["Link"],
-        specialLink: ["hrefLeft", "hrefRight"],
-        aspects: ["invalidHref", "preferButton"],
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
       },
     ],
   },
   overrides: [
     {
-      files: "**/*.{ts,tsx}",
+      files: '**/*.{ts,tsx}',
       extends: [
-        "eason/typescript",
-        "plugin:prettier/recommended", // Make this the last element so prettier config overrides other formatting rules
+        'eason/typescript',
+        'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
       ],
     },
   ],
